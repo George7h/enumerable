ede88239366ee25e4e5997d265a4fa7308636e1f
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-30 14:40:27 +0100

    Add list.rb file

--- my_list.rb
+++ my_list.rb
@@ -1,1 +1,14 @@
 
+require_relative 'enumerable'
+​
+class MyList
+  include MyEnumerable
+​
+  def initialize(*args)
+    @list = args
+  end
+​
+  def each(&block)
+    @list.each(&block)
+  end
+end
