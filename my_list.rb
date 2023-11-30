1b6cab1e5040f57aec4e34af8fd72362c0aca1d4
George <georgie.george7@gmail.com>
2023-11-30 20:16:13 +0400

    fix linters

--- my_list.rb
+++ my_list.rb
@@ -1,12 +1,11 @@
 require_relative 'enumerable'
-​
 class MyList
   include MyEnumerable
-  ​
+
   def initialize(*args)
     @list = args
   end
-  ​
+
   def each(&block)
     @list.each(&block)
   end
