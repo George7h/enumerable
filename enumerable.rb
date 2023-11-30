9fb48d898b3751dbdba9a2bce7894956d7fa9fe4
Austin Obimbo <obimboausts@gmail.com>
2023-11-30 19:01:28 +0300

    Update enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -3,3 +3,15 @@ module MyEnumerable
     each { |item| return false unless yield(item) }
     true
   end
+  
+  def any?()
+    each { |item| return true if yield(item) }
+    false
+  end
+​
+  def filter()
+    result = []
+    each { |item| result << item if yield(item) }
+    result
+  end
+end
