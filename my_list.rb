4517747ae12a3fa2e10d915c3ef35dd57a38f98d
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-30 14:48:26 +0100

    fix linters

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,12 @@
-
 require_relative 'enumerable'
 ​
 class MyList
   include MyEnumerable
-​
+  ​
   def initialize(*args)
     @list = args
   end
-​
+  ​
   def each(&block)
     @list.each(&block)
   end
